b2afedafe0a04d48262d07d30e591933974afbd7
geoffreyd <geoffreyd@gmail.com>
2009-10-09 23:43:15 +1000

    comments and changes added during colaberation.

--- actors/models.py
+++ actors/models.py
@@ -1,39 +1,29 @@
+# actors/models
 from appengine_django.models import BaseModel
 from google.appengine.ext import db
 
-class Client(BaseModel):
+class Actor(BaseModel):
+    agent    = db.ReferenceProperty(Agent) # umm, I don't think I need this. Agent should link back instead? An agent can service more than one actor (though usually it's just the one actor. I think actor's have agents, and not the other way around. IOW An actor only gets one agent, whereas an agent can have multiple actors. So let's store the property here.
+    abilities = db.ReferenceProperty(Ability) #hmm, not sure if we can do, has_many through? yeah, probably not. Maybe a custom index though?
+
+class Client(Actor):
     """docstring for Client"""
-    status            = db.StringProperty()
-    state             = db.TextProperty()
+    state             = db.StringProperty() # a function name
     name              = db.StringProperty()
-    filemakerpro      = db.StringProperty()
-    quickbooks        = db.StringProperty()
-    skus              = db.TextProperty()
     queue_min_size    = db.IntegerProperty()
     queue_min_entries = db.IntegerProperty()
     tags              = db.StringListProperty()
-    
-class Person(BaseModel):
+
+# GD: let's go with whatever the standard is being used by Apple and/or Google contacts
+class Person(Actor):
     kind               = db.StringProperty('type')
-    parent             = db.SelfReferenceProperty()
-    children_count     = db.IntegerProperty()
-    name               = db.StringProperty()
+    first_name         = db.StringProperty()
+    last_name          = db.StringProperty()
     email              = db.EmailProperty()
-    on_private_network = db.BooleanProperty()
-    queue_min_size     = db.IntegerProperty()
+    queue_min_length   = db.IntegerProperty() # in seconds...
     queue_min_entries  = db.IntegerProperty()
-    yaml               = db.TextProperty()
-    state              = db.TextProperty()
-    status             = db.IntegerProperty()
+    state              = db.StringProperty() # a function name
     
-class Contact(Person):
-    
-
-class Contractor(Person):
-    
-
-class Employee(Person):
-    
-
-class Manager(Employee):
+class Executable(Actor):
+    pass
     
--- models.py
+++ models.py
@@ -1,13 +1,15 @@
+# /models
 from appengine_django.models import BaseModel
 from google.appengine.ext import db
 
+# NOTE: the "json" property is not needed throughout (this was a limitation of the older system)
+
 class Ability(BaseModel):
-    actor = db.ReferenceProperty(Actor, collection_name='abilities')
-    role = db.ReferenceProperty(Role, collection_name='abilities')
-    json = db.StringProperty()
-    actor_type = db.StringProperty()
-    state = db.TextProperty()
-    status = db.IntegerProperty()
+    actor   = db.ReferenceProperty(Actor, collection_name='abilities')
+    role    = db.ReferenceProperty(Role, collection_name='abilities')
+    state   = db.TextProperty()
+    status  = db.IntegerProperty()
+    tags    = db.StringListProperty() # this is cool BTW
 
 class Access(BaseModel):
     status        = db.IntegerProperty()
@@ -16,39 +18,22 @@ class Access(BaseModel):
     role          = db.ReferenceProperty(Role)
     tags          = db.StringProperty()
 
-class User(BaseModel):
-    username                    = db.StringProperty()
-    first_name                  = db.StringProperty()
-    last_name                   = db.StringProperty()
-    email                       = db.StringProperty()
+# do we need to have an account model? will this be taken care of by Google Accounts/Django users?
+# class Account(BaseModel):
+#     status
+#     login
+#     email
+#     crypted_password
 
-    # Stored as '[algo]$[salt]$[hexdigest]'
-    password                    = db.StringProperty()
-    is_staff                    = db.BooleanProperty()
-    is_active                   = db.BooleanProperty()
-    is_superuser                = db.BooleanProperty()
-
-    last_login                  = db.DateTimeProperty()
-    date_joined                 = db.DateTimeProperty(auto_add=True)
-    last_updated                = db.DateTimeProperty(auto_add_now=True)
-
-    tags                        = db.StringListProperty()
-
-#    kind                        = db.StringProperty()
-#    status                      = db.IntegerProperty()
-#    salt                        = db.StringProperty()
-#    created_at                  = db.DateTimeProperty()
-#    updated_at                  = db.DateTimeProperty()
-
-# We can use signed cookies for this, storing both the user's key, 
-# and the token expiration date, plus a signature.
-#    remember_token              = db.StringProperty()
-#    remember_token_expires_at   = db.DateTimeProperty()
-
-
-    actor                       = db.ReferenceProperty(Actor)
-    actor_type                  = db.StringProperty()
-    preference                  = db.StringProperty()
+class Agent(BaseModel):
+    actor          = db.ReferenceProperty(Actor)
+    tags           = db.StringListProperty()
+    preference     = db.TextProperty() # stores JavaScript/JSON
+    a              = db.BooleanProperty() # what is 'a'?
+    business       = db.ReferenceProperty(Business) # Actor.kind = Business
+    # has many Abilities
+    # has many Positions
+    # has many Departments through Positions
 
 class AssetDeliverable(BaseModel):
     status      = db.StringProperty()
@@ -64,13 +49,12 @@ class Asset(BaseModel):
     accounting_code = db.StringProperty()
     filemakerpro = db.StringProperty()
     format = db.IntegerProperty()
-    value = db.FloatProperty() # should be Decimal
+    value = db.FloatProperty() # should be Decimal Not sure how GAE handles this.
     scale = db.FloatProperty() # should be Decimal
     path = db.StringProperty()
     name = db.StringProperty()
     parent = db.SelfReferenceProperty()
     client = db.ReferenceProperty(Client)
-    json = db.TextProperty()
     state = db.TextProperty()
     status = db.IntegerProperty()
     children_count = db.IntegerProperty()
@@ -215,48 +199,50 @@ class Issue(BaseModel):
     analysis     = db.TextProperty()
     tags         = db.StringListProperty()
 
-class MailRecipient(BaseModel):
-    """ ... """
-    kind           = db.StringProperty('type')
-    status         = db.IntegerProperty()
-    pending_mail   = db.ReferenceProperty(PendingMail)
-    recipient_key  = db.StringProperty()
-    recipient_type = db.StringProperty()
-    tags           = db.StringListProperty()
+# Since Google has reliable email delivery, we don't need all of the mail-related classes. I used to have
+# a script that parsed the DB transaction logs and made qmail do it's thing. Qmail would report status updates back to the DB.
+#class MailRecipient(BaseModel):
+#    """ ... """
+#    kind           = db.StringProperty('type')
+#    status         = db.IntegerProperty()
+#    pending_mail   = db.ReferenceProperty(PendingMail)
+#    recipient_key  = db.StringProperty()
+#    recipient_type = db.StringProperty()
+#    tags           = db.StringListProperty()
+#
+#class PendingMail(BaseModel):
+#    """ ... """
+#    kind              = db.StringProperty('type')
+#    status            = db.IntegerProperty()
+#    error_code        = db.IntegerProperty()
+#    error_message     = db.StringProperty()
+#    error_type        = db.IntegerProperty()
+#    mailer_template   = db.StringProperty()
+#    mailer_type       = db.StringProperty()
+#    qmail_queue_inode = db.IntegerProperty()
+#    mailable_key      = db.StringProperty()
+#    mailable_type     = db.StringProperty()
+#    sender_key        = db.StringProperty()
+#    sender_type       = db.StringProperty()
+#    tags              = db.StringListProperty()
 
-class PendingMail(BaseModel):
-    """ ... """
-    kind              = db.StringProperty('type')
-    status            = db.IntegerProperty()
-    error_code        = db.IntegerProperty()
-    error_message     = db.StringProperty()
-    error_type        = db.IntegerProperty()
-    mailer_template   = db.StringProperty()
-    mailer_type       = db.StringProperty()
-    qmail_queue_inode = db.IntegerProperty()
-    mailable_key      = db.StringProperty()
-    mailable_type     = db.StringProperty()
-    sender_key        = db.StringProperty()
-    sender_type       = db.StringProperty()
-    tags              = db.StringListProperty()
+# these can be delayed...
+#class PurchaseOrderApplication(BaseModel):
+#    purchase_order    = db.ReferenceProperty(PurchaseOrder)
+#    work_order        = db.ReferenceProperty(WorkOrder)
+#    state             = db.TextProperty()
+#    status            = db.IntegerProperty()
+#
+#class PurchaseOrders(BaseModel):
+#    filemakerpro = db.StringProperty()
+#    quickbooks   = db.StringProperty()
+#    client_id    = db.IntegerProperty()
+#    json         = db.TextProperty()
+#    state        = db.TextProperty()
+#    status       = db.IntegerProperty()
 
-class PurchaseOrderApplication(BaseModel):
-    purchase_order    = db.ReferenceProperty(PurchaseOrder)
-    work_order        = db.ReferenceProperty(WorkOrder)
-    json              = db.TextProperty()
-    state             = db.TextProperty()
-    status            = db.IntegerProperty()
-
-class PurchaseOrders(BaseModel):
-    filemakerpro = db.StringProperty()
-    quickbooks   = db.StringProperty()
-    client_id    = db.IntegerProperty()
-    json         = db.TextProperty()
-    state        = db.TextProperty()
-    status       = db.IntegerProperty()
-
-class Receive(BaseModel):
-    json = db.TextProperty()
+# class Receive(BaseModel):
+#     json = db.TextProperty()
 
 class Requirement(BaseModel):
     name   = db.StringProperty()
@@ -271,19 +257,32 @@ class Resource(BaseModel):
     filemakerpro = db.StringProperty()
     quickbooks   = db.StringProperty()
     location     = db.StringProperty()
-    amount       = db.IntegerProperty() #should be Decimal
+    amount       = db.IntegerProperty() #should be Decimal do this in cents, e.g. 100 = $1
     json         = db.TextProperty()
     state        = db.TextProperty()
     status       = db.IntegerProperty()
 
+# clearly this needs to be flushed out much more... Done.
 class Role(BaseModel):
     """
     Represents a user role and any associated metadata.
     
     """
-    name = db.StringProperty()
-    json = db.TextProperty()
+    parent      = db.SelfReferenceProperty()
+    position    = db.IntegerProperty()
+    status      = db.IntegerProperty()
+    name        = db.StringProperty()
+    tags        = db.StringListProperty()
+    state       = db.TextProperty()
+    # has_many: abilities
+    # has_many :actors, :through => :abilities
+    # has_many :credentials
+    # has_many :actions, :through => :credentials
 
+has_many :constraints
+has_many :rules, :through => :constraints
+
+# this is probably not necessary anymore, now that RBAC is in place
 class SecurityContext(BaseModel):
     """
     Represents <fill in here, please>
@@ -299,26 +298,27 @@ class SecurityContext(BaseModel):
     securable_key   = db.StringProperty()
     securable_model = db.StringProperty()
 
-class Send(BaseModel):
-    json = db.TextProperty()
+# class Send(BaseModel):
+#     json = db.TextProperty()
 
-class TimeSheet(BaseModel):
-    """
-    Represents a user's time sheet for a task
-    
-    """
-    task                = db.ReferenceProperty(Task)
-    person              = db.ReferenceProperty(Person)
-    time_sheet_entry    = db.IntegerProperty()
-    created_on          = db.DateTimeProperty()
-    json                = db.TextProperty()
+# this class is an odd duck. let's leave it out for now.
+# class TimeSheet(BaseModel):
+#     """
+#     Represents a user's time sheet for a task
+#     
+#     """
+#     task                = db.ReferenceProperty(Task)
+#     person              = db.ReferenceProperty(Person)
+#     time_sheet_entry    = db.IntegerProperty()
+#     created_on          = db.DateTimeProperty()
+#     json                = db.TextProperty()
 
-class Transfer(BaseModel):
-    asset    = db.ReferenceProperty(Asset)
-    shipment = db.ReferenceProperty(Shipment)
-    json     = db.TextProperty()
-    state    = db.TextProperty()
-    status   = db.IntegerProperty()
+# class Transfer(BaseModel):
+#     asset    = db.ReferenceProperty(Asset)
+#     shipment = db.ReferenceProperty(Shipment)
+#     json     = db.TextProperty()
+#     state    = db.TextProperty()
+#     status   = db.IntegerProperty()
 
 class Vendor(BaseModel):
     name = db.StringProperty()
